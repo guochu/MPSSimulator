4f2a10b7ddd0893fd7260fc3d85380b3668ff1e9
guo chu <guochu604b@gmail.com>
2022-09-01 10:32:13 +0800

    add adjoint for QubitsOperator

--- src/diff/expecdiff.jl
+++ src/diff/expecdiff.jl
@@ -10,37 +10,39 @@ end
 @adjoint expectation(m::QubitsTerm, state::MPS; kwargs...) = _qterm_expec_util(m, state; kwargs...)
 @adjoint expectation(m::QubitsOperator, state::MPS; kwargs...) = _qop_expec_util(m, state; kwargs...)
 
-function _qterm_expec_util(m::QubitsTerm, state::MPS; trunc::TruncationScheme=DefaultMPSTruncation, alg::AbstractMPSAdd=IterativeAdd(D=trunc.D))
+function _qterm_expec_util(m::QubitsTerm, state::MPS; trunc::TruncationScheme=DefaultMPSTruncation)
 	return expectation(m, state), z -> begin
 		if ishermitian(m)
 			r = copy(state)
 			apply!((2 * real(z)) * m, r; trunc=trunc)
-			canonicalize!(r, normalize=false, trunc=trunc)
+			canonicalize!(r, normalize=false, alg=SVDFact(trunc=trunc))
 		else
 			state_a = copy(state)
 			state_b = copy(state)
 			apply!(conj(z) * m, state_a; trunc=trunc)
 			apply!(z * m', state_b; trunc=trunc)
 			# r = state_a + state_b
+			alg = StableArith(D=trunc.D, ϵ=trunc.ϵ)
 			r, err = mpsadd([state_a, state_b], alg)
 		end
 		return (nothing, r)
 	end 
 end
 
-function _qop_expec_util(m::QubitsOperator, state::MPS; trunc::TruncationScheme=DefaultMPSTruncation, alg::AbstractMPOMPSMult=IterativeMult(D=trunc.D))
+function _qop_expec_util(m::QubitsOperator, state::MPS; trunc::TruncationScheme=DefaultMPSTruncation)
 	return expectation(m, state), z -> begin
+		alg = StableArith(D=trunc.D, ϵ=trunc.ϵ)
 		_is_herm = ishermitian(m)
 		mpo = _MPO(length(state), m)
 		if _is_herm
 			mpsout, err = mpompsmult(mpo, state, alg)
-			mpsout[1] .*= (conj(z) + z)
+			mpsout[1] .*= (2 * real(z))
 		else
 			a, err = mpompsmult(mpo, state, alg)
 			a[1] .*= conj(z)
 			b, err = mpompsmult(mpo', state, alg)
 			b[1] .*= z
-			mpsout, err = mpsadd([a, b], IterativeAdd(D=alg.D, maxiter=alg.maxiter, tol=alg.tol, verbosity=alg.verbosity))
+			mpsout, err = mpsadd([a, b], alg)
 		end
 		return (nothing, mpsout)
 	end 
